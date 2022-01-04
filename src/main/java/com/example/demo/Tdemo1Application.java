751a300c7b7ecc5c759dd8414a3ad270f21f5f53
Mark Fynes <tfynes@vmware.com>
2022-01-04 12:59:35 -0500

    ...

---
+++ src/main/java/com/example/demo/Tdemo1Application.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Tdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tdemo1Application.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("hello tanzu");
+		return "hello tanzu\n";
+	}
+}
